9adc14f1b6e91f4981f7817668cab22239b62f74
deividuska <deividas.krupstas@gmail.com>
2023-04-21 16:42:28 +0100

    Create example.js

---
+++ example.js
@@ -0,0 +1,27 @@
+function validateBusinessEmail(event) {
+  var emailField = document.querySelector('input[name="lepopup-7"]');
+  var emailValue = emailField.value;
+  var businessEmailRegex = /^(?!.*@(gmail\.com|hotmail\.com|yahoo\.com|aol\.com|outlook\.com))[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  if (!businessEmailRegex.test(emailValue)) {
+    event.preventDefault(); // Prevent the form submission
+    emailField.setCustomValidity('Please enter a valid business email address.');
+    emailField.reportValidity();
+    setTimeout(function() {
+      emailField.setCustomValidity('');
+    }, 5000);
+    return false;
+  } else {
+    emailField.setCustomValidity('');
+    return true;
+  }
+}
+
+var submitButton = document.querySelector('.lepopup-button-zoom-out.spu-form');
+submitButton.onclick = function(event) {
+  if (validateBusinessEmail(event)) {
+    return lepopup_submit(this);
+  } else {
+    return false;
+  }
+};
